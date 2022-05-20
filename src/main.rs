67ed997e07a09a237a99bfef9d77062937019d41
Brian Broll <brian.broll@gmail.com>
2022-05-19 21:40:09 -0500

    Fixed some lifetime issues

--- src/main.rs
+++ src/main.rs
@@ -13,50 +13,73 @@ fn is_valid_element(node: &gme::Node, element: &Element) -> bool {
         _ => todo!("attributes, primitives are not yet first class!"),
     }
 }
-//
+
 type ElementIndex = usize;
-fn select_next_element() -> ElementIndex {
+fn select_next_element(
+    _pattern: &Pattern,
+    _assignment: &Assignment,
+    _remaining_elements: &Vec<&Element>,
+) -> ElementIndex {
+    // TODO: Find the element with the most connections to assigned elements
+    0
+}
+
+fn all_nodes(node: &gme::Node) -> Vec<&gme::Node> {
+    let mut nodes = Vec::new();
+    let mut next_nodes = vec![node];
+    while next_nodes.len() > 0 {
+        let next_next_nodes = next_nodes.iter().flat_map(|n| n.children.iter()).collect();
+        nodes.append(&mut next_nodes);
+        next_nodes = next_next_nodes;
+    }
+
     todo!();
 }
 
-fn candidates_for() -> Vec<gme::Node> {
-    todo!();
+fn candidates_for<'a>(
+    node: &'a gme::Node,
+    assignment: &Assignment,
+    element: &Element,
+) -> Vec<&'a gme::Node> {
+    // Find all the valid candidates for the given node
+    // TODO: Optimize this to prioritize child relations, etc
+    let nodes = all_nodes(node);
+    nodes
+        .into_iter()
+        .filter(|node| is_valid_element(node, element))
+        .collect()
 }
-
-// fn () -> Vec<String> {
-//     todo!();
-// }
 
 #[derive(Debug)]
-pub struct Assignment {
-    matches: HashMap<Element, gme::Node>,
+pub struct Assignment<'a> {
+    matches: HashMap<Element, &'a gme::Node>,
 }
 
-impl Assignment {
+impl<'a> Assignment<'a> {
     pub fn new() -> Self {
         Assignment {
             matches: HashMap::new(),
         }
     }
 
-    pub fn with(&self, element: Element, node: gme::Node) -> Self {
+    pub fn with(&self, element: Element, node: &'a gme::Node) -> Self {
         let mut matches = self.matches.clone();
         matches.insert(element, node);
         Self { matches }
     }
 }
 
-pub fn find_assignments(node: &gme::Node, pattern: &Pattern) -> Vec<Assignment> {
+pub fn find_assignments<'a>(node: &'a gme::Node, pattern: &Pattern) -> Vec<Assignment<'a>> {
     let remaining_elements = pattern.reference_elements();
     add_match_to_assignment(node, pattern, Assignment::new(), remaining_elements)
 }
 
-fn add_match_to_assignment(
-    node: &gme::Node,
+fn add_match_to_assignment<'a>(
+    node: &'a gme::Node,
     pattern: &Pattern,
-    partial_assignment: Assignment,
+    partial_assignment: Assignment<'a>,
     mut remaining_elements: Vec<&Element>,
-) -> Vec<Assignment> {
+) -> Vec<Assignment<'a>> {
     // algorithm for finding all assignments:
     let assignments: Vec<_> = Vec::new();
 
@@ -66,11 +89,11 @@ fn add_match_to_assignment(
     }
 
     //  - select an unassigned pattern element: (most connections to resolved nodes?)
-    let idx = select_next_element();
+    let idx = select_next_element(pattern, &partial_assignment, &remaining_elements);
     let element = remaining_elements.swap_remove(idx);
 
     //    - for each candidate for the pattern element:
-    let candidates = candidates_for();
+    let candidates = candidates_for(node, &partial_assignment, &element);
 
     //      - create a new assignment with the candidate and recurse
     for candidate in candidates {
