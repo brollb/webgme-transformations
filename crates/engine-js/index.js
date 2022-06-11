a307c5aeb2cf699f370af8b833e3e819388e82b7
Brian Broll <brian.broll@gmail.com>
2022-06-10 21:10:33 -0500

    WIP Adding JS interface

--- crates/engine-js/index.js
+++ crates/engine-js/index.js
@@ -1,8 +1,55 @@
 const engine = import('./pkg');
 
+function createOutputModel(elements, assignments) {
+	// TODO: how about we create a list of the nodes with the given name
+	// TODO: For each of the assignments, create a new node with the given ID and attribute name
+	const attrRef = assignments[0];
+	console.log(`reference to a node with the given name: ${attrRef}`);
+	// In practice, elements will likely be node paths of the element nodes
+	// This will allow them to easily be turned into a lookup dict
+}
+
 engine.then(m => {
-	console.log({m});
-	const node = new m.GMENode("/path", "hello!");
-	const matches = m.matches(node);
+	// Here is an example of how to find nodes with a given name
+	const parent = new m.GMENode("/path", "parent");
+	parent.add_child(new m.GMENode("/path/1", "target"));
+	parent.add_child(new m.GMENode("/path/2", "target"));
+	const other_child = new m.GMENode("/path/3", "other_child");
+	parent.add_child(other_child);
+	other_child.add_child(new m.GMENode("/path/3/1", "target"));
+
+	const attr = new m.Attribute();
+	const name_const = new m.Constant("name");
+	const target_const = new m.Constant("target");
+	const attr_name_rel = new m.Edge(
+		attr,
+		name_const,
+		new m.Relation.With(m.Property.Name, m.Property.Value)
+	);
+	const attr_val_rel = new m.Edge(
+		attr,
+		target_const,
+		new m.Relation.With(m.Property.Value, m.Property.Value)
+	);
+
+	const elements = [
+		attr,
+		name_const,
+		target_const,
+		attr_val_rel,
+	];
+	const pattern = new m.Pattern(elements);
+
+	// TODO: add elements (as a list?)
+	// TODO: add relations/connectivity. What should these look like?
+	// Find inspiration in jointjs?
+
+	const matches = pattern.matches(node);
+
+	console.log(`found ${matches.length} matches`);
+	matches.forEach(assignments => console.log('\t', assignments));
+
+	// TODO: make an example where we use the assignments to create an output model
+	const models = matches.map(createOutputModel.bind(null, elements);
 })
 	.catch(console.error);
--- crates/engine-js/src/lib.rs
+++ crates/engine-js/src/lib.rs
@@ -35,6 +35,7 @@ pub struct GMENode {
     is_active: Option<bool>,
     is_meta: Option<bool>,
     attributes: HashMap<String, String>, // TODO: add support for other attribute types
+    children: Vec<GMENode>,
 }
 
 #[wasm_bindgen]
@@ -47,14 +48,14 @@ impl GMENode {
             is_active: Some(false),
             is_meta: Some(false),
             attributes,
+            children: Vec::new(),
         }
     }
-}
 
-#[wasm_bindgen]
-pub struct Pattern {
-    //nodes: Vec<Reference>,
-    //edges: Vec<Relation>,
+    #[wasm_bindgen]
+    pub fn add_child(&mut self, child: GMENode) {
+        self.children.push(child);
+    }
 }
 
 impl From<GMENode> for gme::Node {
@@ -82,23 +83,82 @@ impl From<GMENode> for gme::Node {
     }
 }
 
+// pattern components
 #[wasm_bindgen]
-pub fn matches(node: GMENode) {
-    // TODO: change node to JSValue and parse it.
-    // TODO: change node to JSValue and parse it.
-    let node: gme::Node = node.into();
-    let attr_name = gme::AttributeName(String::from("name"));
-    let name = match node
-        .attributes
-        .get(&attr_name)
-        .unwrap_or(&gme::Attribute(Primitive::String(String::from("<none>"))))
-    {
-        gme::Attribute(Primitive::String(name)) => name.to_owned(),
-        _ => String::from("WRONG TYPE"),
-    };
+pub enum Element {
+    Node,
+    Edge,
+}
 
-    log(&format!("called matches with node named: {}", name));
+// TODO: Should I make a generic type that then just records the name and arguments?
+// TODO: a little disappointing but should be ok
+// TODO: add an ID to these automatically? Or just return assignments in the same order?
+#[wasm_bindgen]
+pub struct Attribute;
 
-    // TODO: accept a webgme node in WJI format and a pattern and return a list of assignments
-    //alert("Hello, gme-pattern-engine!");
+#[wasm_bindgen]
+pub struct Constant {
+    value: JsValue,
+}
+
+#[wasm_bindgen]
+pub struct Edge {
+    //src: ,
+    //dst: ,
+    //relation: ,
+}
+
+#[wasm_bindgen]
+pub struct Relation;
+
+#[wasm_bindgen]
+impl Relation {
+    #[wasm_bindgen(constructor)]
+    pub fn With() {
+        todo!();
+    }
+}
+
+#[wasm_bindgen]
+#[derive(Clone)]
+pub struct Pattern {
+    //nodes: Vec<Reference>,
+    //edges: Vec<Relation>,
+}
+
+impl From<Pattern> for pattern::Pattern {
+    fn from(pattern: Pattern) -> Self {
+        todo!();
+    }
+}
+
+#[wasm_bindgen]
+impl Pattern {
+    #[wasm_bindgen(constructor)]
+    pub fn new(elements: Vec<f64>) -> Self {
+        //pub fn new(elements: Vec<Element>) -> Self {
+        // TODO:
+        todo!();
+    }
+
+    #[wasm_bindgen]
+    pub fn matches(&self, node: GMENode) {
+        let node: gme::Node = node.into();
+        let pattern: pattern::Pattern = self.clone().into();
+
+        let attr_name = gme::AttributeName(String::from("name"));
+        let name = match node
+            .attributes
+            .get(&attr_name)
+            .unwrap_or(&gme::Attribute(Primitive::String(String::from("<none>"))))
+        {
+            gme::Attribute(Primitive::String(name)) => name.to_owned(),
+            _ => String::from("WRONG TYPE"),
+        };
+
+        log(&format!("called matches with node named: {}", name));
+
+        // TODO: accept a webgme node in WJI format and a pattern and return a list of assignments
+        //alert("Hello, gme-pattern-engine!");
+    }
 }
