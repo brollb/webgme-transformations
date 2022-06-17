fc333c5a383decf9cf05f2f9e6ce8ab05b8fb88d
Brian Broll <brian.broll@gmail.com>
2022-06-17 14:12:15 -0500

    WIP instantiating nodes (WJI format) from patterns

--- js/src/common/ModelTransformation.js
+++ js/src/common/ModelTransformation.js
@@ -21,8 +21,8 @@ define(['./engine/index'], function(engineModule) {
 			node.setActiveNode();
 			this.steps.reduce(async (refDataP, step) => {
 				const refData = await refDataP;
-				return step.apply(node);
-			}, Promise.resolve());
+				return refData.concat(step.apply(node, activeNode));
+			}, Promise.resolve([]));
 			// TODO
 		}
 
@@ -64,17 +64,22 @@ define(['./engine/index'], function(engineModule) {
 	}
 
 	class TransformationStep {
-		constructor(name, pattern, outputPattern) {
+		constructor(name, core, pattern, outputPattern) {
 			this.name = name;
+			this.core = core;
 			this.pattern = pattern;
 			this.outputPattern = outputPattern;
 		}
 
-		async apply(activeNode) {
+		async apply(node, gmeNode) {
 			console.log('---> applying step', this.name);
-			const matches = await this.pattern.matches(activeNode);
-			const outputs = matches.map(match => this.outputPattern.instantiate(match));
-			// TODO: For each pattern, create the structure
+			const matches = await this.pattern.matches(node);
+			// TODO: how should these be applied?
+			const outputs = await Promise.all(matches.map(
+				(match, index) => this.outputPattern.instantiate(this.core, gmeNode, match, index)
+			));
+			console.log('output of', this.name, JSON.stringify(outputs, null, 2));
+			return outputs;
 		}
 
 		static async fromNode(core, node) {
@@ -87,23 +92,31 @@ define(['./engine/index'], function(engineModule) {
 			]);
 
 			const name = core.getAttribute(node, 'name');
-			return new TransformationStep(name, inputPattern, outputPattern);
+			return new TransformationStep(name, core, inputPattern, outputPattern);
 		}
 	}
 
 	class Pattern {
 		constructor() {
 			this.graph = new Graph();
+			this.externalRelations = [];
+			this.nodePaths = {};
 		}
 
 		async matches(node) {  // TODO: it might be nice to make this synchronous instead...
 			const engine = await getEngine();
-			console.log('(JS) pattern:', JSON.stringify(this));
-			return engine.find_matches(node, this);
+			const assignments = engine.find_matches(node, this.toEnginePattern());
+			return assignments.map(a => mapKeys(a.matches, k => this.nodePaths[k]));
 		}
 
-		addElement(node) {
-			return this.graph.addNode(node);
+		toEnginePattern() {
+			return {graph: this.graph};
+		}
+
+		addElement(node, nodePath) {
+			const index = this.graph.addNode(node);
+			this.nodePaths[index] = nodePath;
+			return index;
 		}
 
 		getElements() {
@@ -114,15 +127,111 @@ define(['./engine/index'], function(engineModule) {
 			return this.graph.addEdge(srcIndex, dstIndex, relation);
 		}
 
+		addCrossPatternRelation(src, dst, relation) {
+			this.externalRelations.push([src, dst, relation]);
+		}
+
+		getRelationsWith(index) {
+			const edges = this.graph.getEdges(index);
+			this.externalRelations.forEach(([src, dst, relation]) => {
+				if (src === index) {
+					edges[0].push([src, dst, relation]);
+				}
+				if (dst === index) {
+					edges[1].push([src, dst, relation]);
+				}
+			});
+			return edges;
+		}
+
 		// TODO: Check if it can be instantiated
 		isInstantiable() {
 			throw new Error('todo!');
 		}
 
-		instantiate(assignments) {
+		async instantiate(core, node, assignments, idPrefix='node') {
+			// TODO: make the WJI format for these
+			console.log(this.externalRelations);
+			const elements = this.getElements().map((element, i) => [element, i]);
+			const [nodeElements, otherElements] = partition(elements, ([e, i]) => e.Node);
+			const nodeIdFor = index => `@id:${idPrefix}_${index}`;
+			const nodes = nodeElements.map(([element, index]) => ({
+				id: nodeIdFor(index),
+				attributes: {},
+				pointers: {},
+			}));
+
+			const updateElements = otherElements.filter(([e, i]) => !e.Constant);
+			await updateElements.reduce(async (prev, [element, index]) => {
+				await prev;
+				const [outEdges, inEdges] = this.getRelationsWith(index);
+				if (element === 'Attribute' || element === 'Pointer') {
+					const [[hasEdge], otherEdges] = partition(inEdges, ([src, dst, relation]) => relation === 'Has');
+					assert(
+						hasEdge,
+						new UninstantiableError(`${element} missing source node ("Has" relation)`)
+					);
+					const nodeWJI = nodes.find(n => n.id === nodeIdFor(hasEdge[0]));
+					const [nameTuple, valueTuple] = getNameValueTupleFor(index, otherEdges.concat(outEdges));
+					const rootNode = core.getRoot(node);
+					const name = await this.resolveNodeProperty(core, rootNode, assignments, ...nameTuple);
+					const targetPath = await this.resolveNodeProperty(core, rootNode, assignments, ...valueTuple);
+					console.log('setting', name, 'to', targetPath);
+					const field = element === 'Attribute' ? 'attributes' : 'pointers';
+					nodeWJI[field][name] = targetPath;
+				} else {
+					throw new Error(`Unsupported element to instantiate: ${element}`);
+				}
+			}, Promise.resolve());
+
 			// TODO: for each node, create it
 			// TODO: sort the nodes by references?
-			console.log('instantiating with', assignments);
+			console.log('instantiating with', JSON.stringify(assignments));
+			return nodes;
+		}
+
+		async resolveNodeProperty(core, rootNode, assignments, indexOrNodePath, property) {
+			const isNodePath = typeof indexOrNodePath === 'string';
+			if (isNodePath) {
+				console.log('RECEIVED NODE PATH!!!!', indexOrNodePath, property, assignments);
+				const node = await core.loadByPath(rootNode, indexOrNodePath);
+				const elementNode = Pattern.getPatternChild(core, node);
+				const elementType = core.getAttribute(core.getMetaType(elementNode), 'name');
+				const elementPath = core.getPath(elementNode);
+				if (elementType === 'Constant') {
+					return core.getAttribute(elementNode, 'value');
+				} else if (elementType.includes('Node')) {
+					return assignments[elementPath].Node;
+				} else if (elementType === 'Attribute') {
+					const [nodeId, attrName] = assignments[elementPath].Attribute;
+					if (property === Property.Name) {
+						return attrName;
+					} else {
+						const targetNode = await core.loadByPath(rootNode, nodeId);
+						return core.getAttribute(targetNode, attrName);
+					}
+				} else {
+					// TODO
+				}
+
+				// TODO: resolve the match?
+				// TODO: convert it to an element?
+				if (property === Property.Name) {
+					return elementPath;
+				} else {
+				}
+			} else {
+				// TODO: resolve the 
+				const element = this.getElements()[indexOrNodePath];
+				if (element.Constant?.Primitive) {
+					return Object.values(element.Constant.Primitive).pop();
+				} else if (element.Constant?.Node) {
+					return element.Constant.Node;
+				} else {
+					console.log(element);
+					assert(false, new Error(`Unknown element type`));
+				}
+			}
 		}
 
 		static async fromNode(core, patternNode) {
@@ -158,7 +267,7 @@ define(['./engine/index'], function(engineModule) {
 						const pointer = Element.Pointer();
 						const ptrName = Element.Constant('base');
 						const base = Element.NodeConstant(baseId);
-						const nodeIndex = pattern.addElement(nodeElement);  // need to add this element first
+						const nodeIndex = pattern.addElement(nodeElement, core.getPath(node));  // need to add this element first
 						const ptrIndex = pattern.addElement(pointer);
 						const ptrNameIndex = pattern.addElement(ptrName);
 						const baseIndex = pattern.addElement(base);
@@ -179,7 +288,7 @@ define(['./engine/index'], function(engineModule) {
 							metaType = 'AnyNode';
 						}
 						const element = Pattern.getElementForNode(core, node, metaType);
-						pattern.addElement(element);
+						pattern.addElement(element, core.getPath(node));
 					}
 				} else {
 					const srcPath = core.getPointerPath(node, 'src');
@@ -193,11 +302,21 @@ define(['./engine/index'], function(engineModule) {
 					const dst = await Endpoint.from(core, patternNode, dstPath, elements[dstIndex]);
 					const relation = Pattern.getRelationElementForNode(core, node, src, dst);
 					// get the index for the element itself (ie, not the port ID)
-					const srcElement = Pattern.getChild(core, patternNode, src.node);
+					const srcElement = Pattern.getPatternChild(core, src.node);
 					const srcElementIndex = elementNodes.findIndex(node => node === srcElement);
-					const dstElement = Pattern.getChild(core, patternNode, dst.node);
+					const dstElement = Pattern.getPatternChild(core, dst.node);
 					const dstElementIndex = elementNodes.findIndex(node => node === dstElement);
-					pattern.addRelation(srcElementIndex, dstElementIndex, relation);
+
+					if (srcElementIndex !== -1 && dstElementIndex !== -1) {
+						pattern.addRelation(srcElementIndex, dstElementIndex, relation);
+					} else {
+						const src = srcElementIndex === -1 ? srcPath : srcElementIndex;
+						const dst = dstElementIndex === -1 ? dstPath : dstElementIndex;
+						console.log('cross pattern relation');
+						const patternPath = core.getPath(patternNode);
+						console.log({src, dst, relation, srcElementIndex, srcPath, patternPath});
+						pattern.addCrossPatternRelation(src, dst, relation);
+					}
 				}
 
 			}, Promise.resolve());
@@ -205,9 +324,13 @@ define(['./engine/index'], function(engineModule) {
 			return pattern;
 		}
 
-		static getChild(core, parent, node) {
+		static getPatternChild(core, node) {
 			let child = node;
-			while (child && core.getParent(child) !== parent) {
+			const isPatternType = n => {
+				const metaType = core.getAttribute(core.getMetaType(n), 'name');
+				return metaType.includes('Pattern') || metaType.includes('Structure');
+			};
+			while (child && !isPatternType(core.getParent(child))) {
 				child = core.getParent(child);
 			}
 			return child;
@@ -268,6 +391,22 @@ define(['./engine/index'], function(engineModule) {
 
 		addEdge(srcIndex, dstIndex, weight) {
 			this.edges.push([srcIndex, dstIndex, weight]);
+		}
+
+		getEdges(index) {
+			const edges = [[], []];
+			this.edges.forEach(edge => {
+				const [src, dst] = edge;
+				if (src === index) {
+					edges[0].push(edge);
+				}
+
+				if (dst === index) {
+					edges[1].push(edge);
+				}
+			});
+
+			return edges;
 		}
 	}
 
@@ -370,5 +509,45 @@ define(['./engine/index'], function(engineModule) {
 		}
 	}
 
+	function partition(list, fn) {
+		const result = [[], []];
+		list.forEach(item => {
+			const index = fn(item) ? 0 : 1;
+			result[index].push(item);
+		});
+		return result;
+	}
+
+	function assert(cond, error = new Error("Assert failed")) {
+		if (!cond) {
+			throw error;
+		}
+	}
+
+	function getNameValueTupleFor(index, edges) {
+		const tuples = new Array(2);
+		edges.filter(([,,relation]) => relation.With)
+			.forEach(edge => {
+				const relation = edge[2];
+				const endpointIndex = edge.indexOf(index);
+				const otherEndpoint = endpointIndex === 0 ? 1 : 0;
+				const nameOrValue = relation.With[endpointIndex];
+				if (nameOrValue === Property.Name) {
+					tuples[0] = [edge[otherEndpoint], relation.With[otherEndpoint]];
+				} else {
+					tuples[1] = [edge[otherEndpoint], relation.With[otherEndpoint]];
+				}
+			});
+
+		return tuples;
+	}
+
+	function mapKeys(obj, fn) {
+		return Object.fromEntries(
+			Object.entries(obj).map(([k, v]) => [fn(k), v])
+		);
+	}
+
+	class UninstantiableError extends Error {}
 	return Transformation;
 });
