3b80753162eb0d63787e11ed5b8c8600d7fc5a28
Brian Broll <brian.broll@gmail.com>
2023-03-02 18:41:45 -0600

    WIP transformation observer

---
+++ src/common/TransformationObserver.js
@@ -0,0 +1,22 @@
+define(["./ModelTransformation"], function (Transformation) {
+  // TODO: select the region for the
+  // Given:
+  //  - transformation
+  //  - observer
+  class TransformationObserver {
+    constructor(client) {
+      this.client = client;
+      // TODO: get the core, too
+    }
+
+    observe(transformation, inputModel) {
+      // TODO: connect the client to the transformation
+    }
+
+    disconnect() {
+      // TODO:
+    }
+  }
+
+  // TODO:
+});
