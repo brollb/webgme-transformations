a431acbed3c703f24ab0d5c45e5ff1fa16bdd3ef
github-action-benchmark <github@users.noreply.github.com>
2023-05-23 16:16:19 +0000

    add Rust Benchmark (cargo) benchmark result for 7f6439c2b6516a24eb74b3ac2258c8dc11027798

---
+++ dev/bench/data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1684858579032,
+  "repoUrl": "https://github.com/brollb/webgme-transformations",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "brollb",
+            "username": "brollb"
+          },
+          "committer": {
+            "name": "brollb",
+            "username": "brollb"
+          },
+          "id": "7f6439c2b6516a24eb74b3ac2258c8dc11027798",
+          "message": "Add performance benchmark lib & example. Close #27",
+          "timestamp": "2023-04-11T21:21:33Z",
+          "url": "https://github.com/brollb/webgme-transformations/pull/28/commits/7f6439c2b6516a24eb74b3ac2258c8dc11027798"
+        },
+        "date": 1684858578318,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "find name of active node (of 500)",
+            "value": 5279,
+            "range": "± 54",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
