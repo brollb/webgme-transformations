2d844bc3d5eea6016c653d3ddb3cfab5504e56a6
github-action-benchmark <github@users.noreply.github.com>
2023-05-23 16:22:36 +0000

    add Rust Benchmark (cargo) benchmark result for 602fc0edf791816a8cff90e32bcea0d8cf5ed822

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1684858579032,
+  "lastUpdate": 1684858956900,
   "repoUrl": "https://github.com/brollb/webgme-transformations",
   "entries": {
     "Rust Benchmark": [
@@ -25,6 +25,36 @@ window.BENCHMARK_DATA = {
             "name": "find name of active node (of 500)",
             "value": 5279,
             "range": "± 54",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "brian.broll@gmail.com",
+            "name": "Brian Broll",
+            "username": "brollb"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "602fc0edf791816a8cff90e32bcea0d8cf5ed822",
+          "message": "Merge pull request #28 from brollb/27-perf-bench\n\nAdd performance benchmark lib & example. Close #27",
+          "timestamp": "2023-05-23T11:18:39-05:00",
+          "tree_id": "a4ee2cefe7c6caa5dce712c4f0f422c12e8f43c8",
+          "url": "https://github.com/brollb/webgme-transformations/commit/602fc0edf791816a8cff90e32bcea0d8cf5ed822"
+        },
+        "date": 1684858955765,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "find name of active node (of 500)",
+            "value": 4756,
+            "range": "± 11",
             "unit": "ns/iter"
           }
         ]
